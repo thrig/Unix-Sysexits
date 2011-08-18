7134082fa1d2b7c763b009383ea4da45488f10f1
Jeremy Mates <jmates@sial.org>
2011-08-18 02:18:16 +0000

    Remove template foo, add it's just sysexits(3) docs.

--- lib/Unix/Sysexits.pm
+++ lib/Unix/Sysexits.pm
@@ -10,36 +10,6 @@ use AutoLoader;
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Unix::Sysexits ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	EX_CANTCREAT
-	EX_CONFIG
-	EX_DATAERR
-	EX_IOERR
-	EX_NOHOST
-	EX_NOINPUT
-	EX_NOPERM
-	EX_NOUSER
-	EX_OK
-	EX_OSERR
-	EX_OSFILE
-	EX_PROTOCOL
-	EX_SOFTWARE
-	EX_TEMPFAIL
-	EX_UNAVAILABLE
-	EX_USAGE
-	EX__BASE
-	EX__MAX
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
 our @EXPORT = qw(
 	EX_CANTCREAT
 	EX_CONFIG
@@ -99,26 +69,26 @@ __END__
 
 =head1 NAME
 
-Unix::Sysexits - Perl extension for blah blah blah
+Unix::Sysexits - Perl extension for sysexits.h
 
 =head1 SYNOPSIS
 
+See sysexits(3) for details on the constants.
+
   use Unix::Sysexits;
-  blah blah blah
+  exit EX_USAGE;
+
+or
+
+  use Unix::Sysexits ();
+  exit Unix::Sysexits::EX_USAGE;
 
 =head1 DESCRIPTION
 
-Stub documentation for Unix::Sysexits, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+No really, just a thin wrapper around sysexits(3) for those constants,
+presumably for exit codes.
 
 =head2 EXPORT
-
-None by default.
-
-=head2 Exportable constants
 
   EX_CANTCREAT
   EX_CONFIG
@@ -139,22 +109,13 @@ None by default.
   EX__BASE
   EX__MAX
 
-
-
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+sysexits(3)
 
 =head1 AUTHOR
 
-jmates, E<lt>jmates@ee.washington.eduE<gt>
+jmates, E<lt>jmates@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
@@ -163,6 +124,5 @@ Copyright (C) 2011 by jmates
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.1 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
